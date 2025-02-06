43ee2cc87b9ee8c9e724b0fdd1eb537575f4f1eb
Mikhail Emeljanov <mishapt@icloud.com>
2025-02-07 01:16:10 +0300

    Шаг 1

---
+++ gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-h, --help', 'output usage information')
+  .action(() => {
+    if (program.opts().help) {
+      program.help();
+    }
+  });
+
+program.parse(process.argv);
